3cdbe46ac2920931a6737545a93ffd4337295ab3
Abhisek Chowdhury <60119622+abhisekChowdhury@users.noreply.github.com>
2026-04-02 22:24:26 -0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memory = {}
+        def dfs(n):
+            if n in memory:
+                return memory[n]
+
+            if n == 0:
+                return 0
+            
+            if n == 1:
+                return 1
+
+            if n == 2:
+                return 2
+            
+            if n == 3:
+                return 3
+                    
+            memory[n] = dfs(n-1) + dfs(n-2)
+
+            return memory[n]
+
+        return dfs(n)
