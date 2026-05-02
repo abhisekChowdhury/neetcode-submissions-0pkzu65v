0070a1e2e72aeb9a6a2b298cecd50ffa71c277fd
Abhisek Chowdhury <60119622+abhisekChowdhury@users.noreply.github.com>
2026-05-01 18:49:02 -0700

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        
+        for idx_current in range(len(temperatures)):
+            for idx_future in range(idx_current,len(temperatures)):
+                if temperatures[idx_future] > temperatures[idx_current]:
+                    #we want the first hot day
+                    answer[idx_current] = idx_future - idx_current
+                    break
+        
+        return answer
