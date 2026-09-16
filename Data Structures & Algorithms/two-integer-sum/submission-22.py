d5289b260731d9c896d93203cce2f10e83f07ad2
Abhisek Chowdhury <60119622+abhisekChowdhury@users.noreply.github.com>
2026-09-16 12:54:47 -0700

    Add: two-integer-sum - submission-22

---
+++ Data Structures & Algorithms/two-integer-sum/submission-22.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+
+        for i in range(len(nums)):
+            remaining = target - nums[i]
+
+            if remaining in visited:
+                return [visited[remaining],i]
+            
+            visited[nums[i]] = i
+        
+        return []
